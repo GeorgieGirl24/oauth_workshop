6267c5d6b595b97094fea7022c44a349955e0060
George Soderholm <georgesoderholm@gmail.com>
2021-01-02 22:22:21 -0700

    Refactor

--- app/services/repo_service.rb
+++ app/services/repo_service.rb
@@ -1,15 +1,19 @@
 class RepoService
-  def self.conn(current_user)
-    Faraday.new(url: 'https://api.github.com', headers: { 'Authorization': "token #{current_user.token}"})
-  end
-
   def self.private_repos(current_user)
     response = conn(current_user).get('/user/repos?per_page=100&type=private')
-    JSON.parse(response.body, symbolize_names: true)
+    get_parsed(response)
   end
 
   def self.public_repos(current_user)
     response = conn(current_user).get('/user/repos?per_page=100&type=public')
+    get_parsed(response)
+  end
+
+  private
+  def self.get_parsed(response)
     JSON.parse(response.body, symbolize_names: true)
+  end
+  def self.conn(current_user)
+    Faraday.new(url: 'https://api.github.com', headers: { 'Authorization': "token #{current_user.token}"})
   end
 end
