d8fb94c26b8fed8a6c869261ce4b5f3c5f9d7319
George Soderholm <georgesoderholm@gmail.com>
2021-01-03 17:36:37 -0700

    Add omniauth to the application

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,18 @@ class SessionsController < ApplicationController
     access_token = data[:access_token]
 
     user_data = SearchFacade.user_access(access_token)
-
-    user = User.find_or_create_by(uid: user_data[:id])
-    user.username = user_data[:login]
-    user.uid = user_data[:id]
-    user.token = access_token
-    user.save
-
-    session[:user_id] = user.id
-
-    redirect_to dashboard_path
+    user = User.find_or_create_user(auth_hash)
+    if user
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
   end
 
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  def self.find_or_create_user(auth)
+    user = User.find_or_create_by(uid: auth['uid'])
+    user.username = auth['info']['nickname']
+    user.uid = auth[:id]
+    user.token = auth['credentials']['token']
+    user.save
+    user
+  end
 end
